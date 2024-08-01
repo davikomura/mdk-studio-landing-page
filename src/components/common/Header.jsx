dd565b5d9046fbeb69d657ea7a6f050173f2b99e
Daniel Lima Azevedo <d.lima@acad.ifma.edu.br>
2024-08-01 20:08:40 -0300

    reafatoracao de footer header e about

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/image.png'
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        console.log("teste")
+        setMenuOpen(!menuOpen);
+    };
+
     return(
        
         <header className='fixed top-0 left-0 right-0 flex  font-sans min-h-[70px] tracking-wide z-50'>
@@ -11,7 +19,7 @@ const Header = () => {
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                             {/* Mobile menu button */}
-                            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                            <button type="button" onClick={toggleMenu} className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                             <span className="absolute -inset-0.5"></span>
                             <span className="sr-only">Open main menu</span>
                                 {/* Icon when menu is closed.
@@ -50,20 +58,12 @@ const Header = () => {
                         </div>
                         </div>
                     </div>
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                            <span className="absolute -inset-1.5"></span>
-                            <span className="sr-only">View notifications</span>
-                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                            </svg>
-                        </button>
-                    </div>
+                   
                     </div>
                 </div>
 
                 {/* Mobile menu, show/hide based on menu state. */}
-                <div className="sm:hidden" id="mobile-menu">
+                <div className={`sm:hidden ${menuOpen ? 'block' : 'hidden'}`}  id="mobile-menu">
                     <div className="space-y-1 px-2 pb-3 pt-2">
                         {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                         <Link to="/planos">
@@ -87,38 +87,3 @@ const Header = () => {
 }
 
 export default Header
-
-
-
-
-
-{/* <div id="menu" className='hidden md:block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50'>
-                  
-
-<ul className='hidden w-full md:block md:w-auto lg:flex gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50  justify-center'>
-    <li className='mb-6 hidden max-lg:block'>
-        <a href="javascript:void(0)"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' /></a>
-    </li>
-    <li className='max-lg:border-b max-lg:py-3 px-3'>
-        <Link to="/planos">
-            <a href='javascript:void(0)' className='hover:text-[#007bff] text-white font-bold block text-xl'>Planos</a>
-        </Link>
-    </li>
-    <li className='max-lg:border-b max-lg:py-3 px-3'>
-        <a href='javascript:void(0)'
-        className='hover:text-[#007bff] text-white font-bold block text-xl'>Galeria</a>
-    </li>
-    <li className='max-lg:border-b max-lg:py-3 px-3'>
-        <Link to="/about">
-            <a href='javascript:void(0)'
-            className='hover:text-[#007bff] text-white font-bold block text-xl'>Sobre Nós</a>
-        </Link>
-    </li>
-    <li className='max-lg:border-b max-lg:py-3 px-3'>
-        <Link to="/FAQ">
-            <a href='javascript:void(0)'
-            className='hover:text-[#007bff] text-white font-bold block text-xl'>FAQ</a>
-        </Link>
-    </li>
-</ul>
-</div> */}
