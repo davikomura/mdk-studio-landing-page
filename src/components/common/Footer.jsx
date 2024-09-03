4459c9e7c87029afa35b1e10ac113f773d20c396
Felipe Macedo <141750211+macedoflp@users.noreply.github.com>
2024-09-03 15:36:37 +0000

    remocao de formulario e adicao de botoes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,9 +3,9 @@ import zapzap from '../../assets/wpp.png'
 
 const Footer = () => {
     return(
-        <div className='bg-transparent fixed bottom-0 right-0 z-50 pr-12 pb-12 w-20 md:w-25 lg:w-28'>
-            <img href = "#" src={zapzap}/>
-        </div>
+        <a href='#' className='bg-transparent fixed bottom-0 right-0 z-50 pr-12 pb-12 w-20 md:w-25 lg:w-28'>
+            <img src={zapzap}/>
+        </a>
     )
 }
 
