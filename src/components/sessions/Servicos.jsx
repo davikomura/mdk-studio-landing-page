f797721a1716ac71f01f6021ed9b438049caf785
Felipe Macedo <141750211+macedoflp@users.noreply.github.com>
2024-09-03 14:09:29 +0000

    mudanças no servico

--- src/components/sessions/Servicos.jsx
+++ src/components/sessions/Servicos.jsx
@@ -4,8 +4,10 @@ export const Servicos = () => {
   return (
     <div id="Servicos" className="bg-black w-full min-h-screen inset-0">
 
-        <div className="relative z-10 pt-40">
+        <div className=" relative z-10 pt-40">
             <h2 className="text-xl text-white font-bold leading-tight text-center sm:text-5xl">Serviços</h2>
+            <p className='text-center mt-8 text-gray-200 flex justify-center'>Na MDK, transformamos ideias em experiências digitais incríveis. Oferecemos uma gama completa de serviços para atender às suas necessidades de tecnologia,
+                <br/> garantindo que cada projeto seja um sucesso do início ao fim.</p>
         </div>
 
         <div className="grid md:grid-cols-3 max-w-screen-lg mx-auto gap-10 mt-16 px-5">
@@ -13,18 +15,28 @@ export const Servicos = () => {
             <div className="flex gap-4 items-start flex-col ">
                 <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"><path d="M9.96424 2.68571C10.0668 2.42931 9.94209 2.13833 9.6857 2.03577C9.4293 1.93322 9.13832 2.05792 9.03576 2.31432L5.03576 12.3143C4.9332 12.5707 5.05791 12.8617 5.3143 12.9642C5.5707 13.0668 5.86168 12.9421 5.96424 12.6857L9.96424 2.68571ZM3.85355 5.14646C4.04882 5.34172 4.04882 5.6583 3.85355 5.85356L2.20711 7.50001L3.85355 9.14646C4.04882 9.34172 4.04882 9.6583 3.85355 9.85356C3.65829 10.0488 3.34171 10.0488 3.14645 9.85356L1.14645 7.85356C0.951184 7.6583 0.951184 7.34172 1.14645 7.14646L3.14645 5.14646C3.34171 4.9512 3.65829 4.9512 3.85355 5.14646ZM11.1464 5.14646C11.3417 4.9512 11.6583 4.9512 11.8536 5.14646L13.8536 7.14646C14.0488 7.34172 14.0488 7.6583 13.8536 7.85356L11.8536 9.85356C11.6583 10.0488 11.3417 10.0488 11.1464 9.85356C10.9512 9.6583 10.9512 9.34172 11.1464 9.14646L12.7929 7.50001L11.1464 5.85356C10.9512 5.6583 10.9512 5.34172 11.1464 5.14646Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
                 <div>
-                    <h3 className="text-white font-semibold text-xl"> Expressive API</h3>
-                    <p className="mt-1 text-gray-500"> You don't need to be an expert to use our plugin. Our expressive API is
-                        readable and well documented.</p>
+                    <h3 className="text-white font-semibold text-xl"> Desenvolvimento de Sites</h3>
+                    <p className="mt-1 text-gray-400 text-balance"> <strong>Seu site pode ser o vendedor mais eficiente da sua empresa.</strong> Criamos sites que são verdadeiras máquinas de conversão, otimizados para captar leads e fechar vendas.</p>
+                    <a href='#' className='mt-1 justify-center flex items-center text-sm text-blue-400 underline underline-offset-2 hover:scale-110 transition-transform duration-200'>
+                        Saiba Mais
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" class="rotate-180 stroke-blue-400">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" d="M11 6L5 12M5 12L11 18M5 12H19"></path>
+                        </svg>
+                    </a>
                 </div>
             </div>
 
             <div className="flex gap-4 items-start flex-col ">
                 <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"><path d="M0.849976 1.74998C0.849976 1.25292 1.25292 0.849976 1.74998 0.849976H3.24998C3.74703 0.849976 4.14998 1.25292 4.14998 1.74998V2.04998H10.85V1.74998C10.85 1.25292 11.2529 0.849976 11.75 0.849976H13.25C13.747 0.849976 14.15 1.25292 14.15 1.74998V3.24998C14.15 3.74703 13.747 4.14998 13.25 4.14998H12.95V10.85H13.25C13.747 10.85 14.15 11.2529 14.15 11.75V13.25C14.15 13.747 13.747 14.15 13.25 14.15H11.75C11.2529 14.15 10.85 13.747 10.85 13.25V12.95H4.14998V13.25C4.14998 13.747 3.74703 14.15 3.24998 14.15H1.74998C1.25292 14.15 0.849976 13.747 0.849976 13.25V11.75C0.849976 11.2529 1.25292 10.85 1.74998 10.85H2.04998V4.14998H1.74998C1.25292 4.14998 0.849976 3.74703 0.849976 3.24998V1.74998ZM2.94998 4.14998V10.85H3.24998C3.74703 10.85 4.14998 11.2529 4.14998 11.75V12.05H10.85V11.75C10.85 11.2529 11.2529 10.85 11.75 10.85H12.05V4.14998H11.75C11.2529 4.14998 10.85 3.74703 10.85 3.24998V2.94998H4.14998V3.24998C4.14998 3.74703 3.74703 4.14998 3.24998 4.14998H2.94998ZM2.34998 1.74998H1.74998V2.34998V2.64998V3.24998H2.34998H2.64998H3.24998V2.64998V2.34998V1.74998H2.64998H2.34998ZM5.09998 5.99998C5.09998 5.50292 5.50292 5.09998 5.99998 5.09998H6.99998C7.49703 5.09998 7.89998 5.50292 7.89998 5.99998V6.99998C7.89998 7.03591 7.89787 7.07134 7.89378 7.10618C7.92861 7.10208 7.96405 7.09998 7.99998 7.09998H8.99998C9.49703 7.09998 9.89998 7.50292 9.89998 7.99998V8.99998C9.89998 9.49703 9.49703 9.89998 8.99998 9.89998H7.99998C7.50292 9.89998 7.09998 9.49703 7.09998 8.99998V7.99998C7.09998 7.96405 7.10208 7.92861 7.10618 7.89378C7.07134 7.89787 7.03591 7.89998 6.99998 7.89998H5.99998C5.50292 7.89998 5.09998 7.49703 5.09998 6.99998V5.99998ZM6.09998 5.99998H5.99998V6.09998V6.89998V6.99998H6.09998H6.89998H6.99998V6.89998V6.09998V5.99998H6.89998H6.09998ZM7.99998 7.99998H8.09998H8.89998H8.99998V8.09998V8.89998V8.99998H8.89998H8.09998H7.99998V8.89998V8.09998V7.99998ZM2.64998 11.75H2.34998H1.74998V12.35V12.65V13.25H2.34998H2.64998H3.24998V12.65V12.35V11.75H2.64998ZM11.75 1.74998H12.35H12.65H13.25V2.34998V2.64998V3.24998H12.65H12.35H11.75V2.64998V2.34998V1.74998ZM12.65 11.75H12.35H11.75V12.35V12.65V13.25H12.35H12.65H13.25V12.65V12.35V11.75H12.65Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
                 <div>
-                    <h3 className="text-white font-semibold text-xl">Highly performant</h3>
-                    <p className="mt-1 text-gray-500"> You can make sure your website or app is highly performant with a built-in
-                        system to help you optimize.</p>
+                    <h3 className="text-white font-semibold text-xl">Desenvolvimento de Sistemas</h3>
+                    <p className="mt-1 text-gray-400 text-balance"> <strong>Sistemas que fazem sua empresa crescer mais rápido.</strong> Desenvolvemos soluções sob medida para tornar sua operação mais eficiente e lucrativa. Não fique para trás, implemente a tecnologia que vai impulsionar seu negócio.</p>
+                    <a href='#' className='mt-1 justify-center flex items-center text-sm text-blue-400 underline underline-offset-2 hover:scale-110 transition-transform duration-200'>
+                        Saiba Mais
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" class="rotate-180 stroke-blue-400">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" d="M11 6L5 12M5 12L11 18M5 12H19"></path>
+                        </svg>
+                    </a>
                 </div>
             </div>
 
@@ -32,13 +44,31 @@ export const Servicos = () => {
             <div className="flex gap-4 items-start flex-col ">
                 <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5"><path d="M7.28856 0.796908C7.42258 0.734364 7.57742 0.734364 7.71144 0.796908L13.7114 3.59691C13.8875 3.67906 14 3.85574 14 4.05V10.95C14 11.1443 13.8875 11.3209 13.7114 11.4031L7.71144 14.2031C7.57742 14.2656 7.42258 14.2656 7.28856 14.2031L1.28856 11.4031C1.11252 11.3209 1 11.1443 1 10.95V4.05C1 3.85574 1.11252 3.67906 1.28856 3.59691L7.28856 0.796908ZM2 4.80578L7 6.93078V12.9649L2 10.6316V4.80578ZM8 12.9649L13 10.6316V4.80578L8 6.93078V12.9649ZM7.5 6.05672L12.2719 4.02866L7.5 1.80176L2.72809 4.02866L7.5 6.05672Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
                 <div>
-                    <h3 className="text-white font-semibold text-xl">No dependencies </h3>
-                    <p className="mt-1 text-gray-500"> Our plugins do not have any external dependencies so our plugin has the
-                        minimal footprint possible.</p>
+                    <h3 className="text-white font-semibold text-xl">Design UX/UI </h3>
+                    <p className="mt-1 text-gray-400 text-balance"> <strong>Clientes encantados voltam sempre.</strong> Nossos designs não são apenas bonitos, são estrategicamente pensados para prender a atenção e converter visitantes em clientes fiéis.</p>
+                    <a href='#' className='mt-1 justify-center flex items-center text-sm text-blue-400 underline underline-offset-2 hover:scale-110 transition-transform duration-200'>
+                        Saiba Mais
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24" class="rotate-180 stroke-blue-400">
+                            <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" d="M11 6L5 12M5 12L11 18M5 12H19"></path>
+                        </svg>
+                    </a>
                 </div>
             </div>
         
         </div>
+        <button class="top-32 font-sans flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group" type="submit">
+        Solicitar Orçamento
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 19"
+                class="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+            >
+                <path
+                class="fill-gray-800 group-hover:fill-gray-800"
+                d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+                ></path>
+            </svg>
+        </button>
         <hr class="h-px mt-64 bg-gray-200 border-0 dark:bg-gray-700 relative"></hr>
     </div>
   )
