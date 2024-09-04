dde5adc9b1bc4b5dd462b04fe7fc82f730f54f56
Davi Komura de Castro <davi.komura@gmail.com>
2024-09-04 19:21:16 +0100

    Adição de links de whatsapp

--- src/components/sessions/Home.jsx
+++ src/components/sessions/Home.jsx
@@ -14,8 +14,10 @@ const Home = () => {
                 <p className="text-white text-center text-2xl mb-24">
                     Sua presença online inicia aqui
                 </p>
-                <button className='flex text-xl justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group'>
-                    Comece Já</button>
+                <button className='flex text-xl justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group'
+                        onClick={() => window.location.href = 'https://w.app/0wj9vU'}>
+                    Comece Já
+                </button>
             </div>
            
         </div>
