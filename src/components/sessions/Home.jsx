a4eabd49925be32da1b751de2f5ff4ff2f914538
DanielKorban <74277609+daniel99korban@users.noreply.github.com>
2024-09-13 23:17:26 +0000

    teste3

--- src/components/sessions/Home.jsx
+++ src/components/sessions/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
+import homeImg from '../../assets/img1.jpg';
 
 
 const Home = () => {
     return(
-        <div id="home" className=" relative inset-0 bg-cover bg-center h-screen" style={{backgroundImage: "url('../assets/img1.jpg')"}}>
+    <div id="home" className=" relative inset-0 bg-cover bg-center h-screen" style={{backgroundImage: "url("+homeImg+")"}}>
             <div className="absolute w-full h-full bg-black opacity-50" />
             <div className="relative flex z-10 flex-col justify-center items-center h-screen">
                 <h1 className="text-white text-center text-4xl font-bold mb-4 lg:text-5xl">
                     Mostre seu trabalho para o mundo 
                     <br />
-                    com um site personalizadooooooooooo
+                    com um site personalizado teste
                 </h1>
                 <p className="text-white text-center text-2xl mb-24">
                     Sua presença online inicia aqui
