d0386c4983e1552aa1e25a13dafc9926fde80352
Davi Komura de Castro <davi.komura@gmail.com>
2024-09-14 16:19:14 +0100

    Correcao hover

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
                 <div className={`sm:hidden ${menuOpen ? 'block' : 'hidden'}`}  id="mobile-menu">
                     <div className="space-y-1 px-2 pb-3 pt-2">
                         {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                        <a href="#servicos" className="block rounded-md bg-white-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Serviços</a>
+                        <a href="#servicos" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Serviços</a>
                         <a href="#galeria" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Galeria</a>
                         <a href="#about" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Sobre Nós</a>
                         <a href="#FAQ" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">FAQ</a>
