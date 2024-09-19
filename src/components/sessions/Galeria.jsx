f06b0e94be4d2b824d95576273ad47f34d7d56cf
Davi Komura de Castro <davi.komura@gmail.com>
2024-09-19 21:21:20 +0100

    Correcoes gerais no site

--- src/components/sessions/Galeria.jsx
+++ src/components/sessions/Galeria.jsx
@@ -3,15 +3,15 @@ import ImageCarousel from '../content/ImageCarousel';
 
 export const Galeria = () => {
   return (
-    <div id="galeria" className="bg-black object-cover object-center relative inset-0 bg-cover bg-center h-screen">
+    <div id="galeria" className="bg-black object-cover object-center relative inset-0 bg-cover bg-center">
       {/* Background image container */}
       
       {/* Content container */}
-      <div className="relative pt-40">
+      <div className="relative pt-40 pb-20">
         <h2 className="text-xl text-white font-bold leading-tight text-center sm:text-5xl">Galeria</h2>
         <ImageCarousel />
       </div>
-      <hr class="h-px mt-64 bg-gray-200 border-0 dark:bg-gray-700 relative"></hr>
+      <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700"></hr>
     </div>
   );
 }
